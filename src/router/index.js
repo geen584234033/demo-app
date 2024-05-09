7806d0b68e8e12790844752ea4b27ff7b28fc262
LAPTOP-CMKS6OPG\User <geenlufy@gmail.com>
2024-05-10 00:24:02 +0700

    [fix]: remove log

--- src/router/index.js
+++ src/router/index.js
@@ -16,16 +16,6 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
-  scrollBehavior(to) {
-    console.log(to);
-    if (to.hash === "#d") {
-      return {
-        selector: to.hash,
-        offset: { x: 0, y: 1000 },
-      };
-    }
-    return { x: 0, y: 0 };
-  },
 });
 
 export default router;
